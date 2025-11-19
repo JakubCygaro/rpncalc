8434c0ec2380b6a6fc45d74ceae072c7256913f9
papieros <robertrugala@gmail.com>
2025-11-19 11:59:26 +0100

    fix typo

--- rpncalc.hs
+++ rpncalc.hs
@@ -1,15 +1,15 @@
-import Text.Read
-import System.IO
-import System.IO.Error
 import Control.Exception
 import GHC.Float (powerFloat)
+import System.IO
+import System.IO.Error
+import Text.Read
 
 type Stack = [Float]
 
 main = catch (rpn []) handler
-    where
-        handler :: IOError -> IO ()
-        handler e = putStrLn $ "Runtime error: " ++ show e
+  where
+    handler :: IOError -> IO ()
+    handler e = putStrLn $ "Runtime error: " ++ show e
 
 onError :: IOError -> IO ()
 onError e
@@ -27,50 +27,52 @@ rpn stack = do
 
 performOperation :: [String] -> Stack -> IO Stack
 performOperation [] stack = return stack
-performOperation (t:tokensRest) stack = do
+performOperation (t : tokensRest) stack = do
     stack' <- case (readMaybe t :: Maybe Float) of
-            Just n -> return (n:stack)
-            Nothing -> case t of
-                "+" -> binaryOp (+) stack
-                "-" -> binaryOp (flip (-)) stack
-                "*" -> binaryOp (*) stack
-                "**" -> binaryOp powerFloat stack
-                "/" -> binaryOp (flip (/)) stack
-                "%" -> binaryOp (\a b -> fromIntegral $ round b  `mod` round a) stack
-                "&" -> flipFlop stack
-                "_" -> unaryOp (fromIntegral . floor) stack
-                "^" -> unaryOp (\a -> fromIntegral $ floor $ a + 1) stack
-                "!" -> return $ drop 1 stack
-                "!!!" -> return []
-                "dup" -> return $ head stack : stack
-                "$" -> return [sum stack]
-                _ -> ioError $ userError $ "Unknown operator `" ++ t ++ "`"
+        Just n -> return (n : stack)
+        Nothing -> case t of
+            "+" -> binaryOp (+) stack
+            "-" -> binaryOp (flip (-)) stack
+            "*" -> binaryOp (*) stack
+            "**" -> binaryOp powerFloat stack
+            "/" -> binaryOp (flip (/)) stack
+            "%" -> binaryOp (\a b -> fromIntegral $ round b `mod` round a) stack
+            "&" -> flipFlop stack
+            "_" -> unaryOp (fromIntegral . floor) stack
+            "^" -> unaryOp (\a -> fromIntegral $ floor $ a + 1) stack
+            "!" -> return $ drop 1 stack
+            "!!!" -> return []
+            "dup" -> return $ head stack : stack
+            "$" -> return [sum stack]
+            _ -> ioError $ userError $ "Unknown operator `" ++ t ++ "`"
     performOperation tokensRest stack'
 
-takeOperansFromStack :: Int -> Stack -> (Maybe [Float], Stack)
-takeOperansFromStack n stack
-    | len >= n = let ops = take n stack
-                     stack' = drop n stack
-                 in (Just ops, stack')
+takeOperandsFromStack :: Int -> Stack -> (Maybe [Float], Stack)
+takeOperandsFromStack n stack
+    | len >= n =
+        let ops = take n stack
+            stack' = drop n stack
+         in (Just ops, stack')
     | otherwise = (Nothing, stack)
-        where len = length stack
+  where
+    len = length stack
 
 unaryOp :: (Float -> Float) -> Stack -> IO Stack
 unaryOp op stack =
-    let (operand, stack') = takeOperansFromStack 1 stack
-    in case operand of
-        Nothing -> ioError $ userError "not enough arguments for binary operation"
-        Just [a] -> return $ op a : stack'
+    let (operand, stack') = takeOperandsFromStack 1 stack
+     in case operand of
+            Nothing -> ioError $ userError "not enough arguments for binary operation"
+            Just [a] -> return $ op a : stack'
 
 binaryOp :: (Float -> Float -> Float) -> Stack -> IO Stack
 binaryOp op stack =
-    let (operands, stack') = takeOperansFromStack 2 stack
-    in case operands of
-        Nothing -> ioError $ userError "not enough arguments for binary operation"
-        Just [a, b] -> return $ (a `op` b) : stack'
+    let (operands, stack') = takeOperandsFromStack 2 stack
+     in case operands of
+            Nothing -> ioError $ userError "not enough arguments for binary operation"
+            Just [a, b] -> return $ (a `op` b) : stack'
 
 flipFlop stack =
-    let (operands, stack') = takeOperansFromStack 2 stack
-    in case operands of
-        Nothing -> ioError $ userError "not enough arguments for flip operation"
-        Just [a, b] -> return $ b : a : stack'
+    let (operands, stack') = takeOperandsFromStack 2 stack
+     in case operands of
+            Nothing -> ioError $ userError "not enough arguments for flip operation"
+            Just [a, b] -> return $ b : a : stack'
